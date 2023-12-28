7efd381dc0e594f52750ec503d61b07d4437b188
NGUYEN KE AN <nkalk192002@gmail.com>
2023-12-28 13:00:18 +0100

    fix: remove slash for url in cors list

--- frontend/src/services/api/BaseService.ts
+++ frontend/src/services/api/BaseService.ts
@@ -5,9 +5,9 @@ import appConfig from "../../configs/APIConfig";
 const BaseService = axios.create({
     timeout: 60000,
     baseURL: appConfig.apiBaseURL,
-    params: {
-        key: appConfig.apiKey
-    }
+    // params: {
+    //     key: appConfig.apiKey
+    // }
 })
 
 export default BaseService;
