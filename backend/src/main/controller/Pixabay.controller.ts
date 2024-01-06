790c5ec0d1a47301bcd94d6a64e685b980b2ff20
NGUYEN KE AN <nkalk192002@gmail.com>
2024-01-06 22:22:04 +0100

    UPDATE: README.md

--- backend/src/main/controller/Pixabay.controller.ts
+++ backend/src/main/controller/Pixabay.controller.ts
@@ -6,8 +6,10 @@ import PixabayService from "../services/Pixabay.service";
 
 let getImagesByQuery = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     if (typeof req.query.q !== 'string') return res.status(200).send("Query is missing");
+    let page = "1";
+    if(typeof req.query.page === "string") page = req.query.page;
     try {
-        const data = await PixabayService.searchImage(PixabayService.SearchStrategy.WITH_CACHE, req.query.q);
+        const data = await PixabayService.searchImage(PixabayService.SearchStrategy.WITH_CACHE, req.query.q, page);
         return res.status(200).json(data);
     } catch (error: any) {
         if (error.request) return next(new AxiosRequestError(error));
